2b302f6303eaf03d7a023bfd19c7b8a8169a2346
igor kapusniak <20102236@gmail.com>
2024-03-14 23:06:50 +0000

    Added image to imageview

--- src/main/java/com/example/parisroutefinder/HelloController.java
+++ src/main/java/com/example/parisroutefinder/HelloController.java
@@ -2,13 +2,33 @@ package com.example.parisroutefinder;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    FileInputStream input;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    {
+        try {
+            input = new FileInputStream("src/main/resources/Image/gimp.png");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private Image parisMap = new Image(input);
+    @FXML
+    public ImageView imageView;
+
+    public void initialize(){
+        imageView.setImage(parisMap);
+    }
+
 }
